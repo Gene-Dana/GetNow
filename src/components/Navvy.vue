dad3e89726ac2cd2bae00447eb667dbf396f0d0b
Gene <yllanesgene@gmail.com>
2019-08-07 20:12:42 -0400

    Update Navvy.vue

--- src/components/Navvy.vue
+++ src/components/Navvy.vue
@@ -6,7 +6,7 @@
 
 			<v-toolbar-title>Get <span class="blue--text text--darken-3"> Medical Insurance </span> Now</v-toolbar-title>
 			<v-spacer></v-spacer>
-			<v-toolbar-items class="hidden-xs-and-down" color="blue">
+			<v-toolbar-items class="hidden-sm-and-down" color="blue">
 				<v-btn color="blue"><span class="title align-end white--text">(877) 342-4747</span></v-btn>
 
 			</v-toolbar-items>
